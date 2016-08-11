4cf3d75a9dca421d0eaeb0414e1803a904a23896
TobyMaxham <tobias.maxham@tricept.de>
2016-08-11 22:34:54 +0200

    Upload/Backup is now ready for use

--- src/BackblazeManager.php
+++ src/BackblazeManager.php
@@ -1,11 +1,23 @@
 <?php
 
+/*
+ * This file is part of Laravel Junkies Backblaze.
+ *
+ * Copyright (c) 2016 Tobias Maxham <git2016@maxham.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ */
+
 namespace LaraJunkies\Backblaze;
 
 /**
  * Class BackblazeManager
  * @package LaraJunkies\Backblaze
- * @author Tobias Maxham
+ *
+ * @author Tobias Maxham <git2016@maxham.de>
  */
 class BackblazeManager
 {
@@ -15,17 +27,51 @@ class BackblazeManager
      */
     private $b2API;
 
+
     private $AUTH_TOKEN;
 
+    private $UPLOAD_AUTH_TOKEN;
+
     private $ACCOUNT_ID;
+
+    private $DOWNLOAD_URL;
+
+    private $UPLOAD_URL;
+
+    private $APPLICATION_KEY;
 
     private $BUCKET_ID;
 
     private $API_URL;
 
-    public function __construct($b2API)
+    /**
+     * BackblazeManager constructor.
+     * @param $b2API
+     * @param array $config
+     */
+    public function __construct($b2API, $config = [])
     {
         $this->b2API = $b2API;
+
+        if (isset($config['ACCOUNT_ID'])) {
+            $this->ACCOUNT_ID = $config['ACCOUNT_ID'];
+        }
+
+        if (isset($config['APPLICATION_KEY'])) {
+            $this->APPLICATION_KEY = $config['APPLICATION_KEY'];
+        }
+
+        if (isset($config['BUCKET_ID'])) {
+            $this->BUCKET_ID = $config['BUCKET_ID'];
+        }
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (!method_exists($this->api(), $name)) {
+            throw new \Exception('Method "' . $name . '" not found. Invalid callback for ' . self::class);
+        }
+        return call_user_func_array([$this->api(), $name], $arguments);
     }
 
     /**
@@ -36,14 +82,59 @@ class BackblazeManager
         return $this->b2API;
     }
 
-    public function uploadURL()
+    /**
+     * @param null $accountID
+     * @param null $appKey
+     * @return \stdClass
+     */
+    public function authorize($accountID = null, $appKey = null)
     {
-        return $this->api()->b2_get_upload_url($this->API_URL, $this->ACCOUNT_ID, $this->AUTH_TOKEN, $this->BUCKET_ID);
+        if (null == $accountID) $accountID = $this->ACCOUNT_ID;
+        else $this->ACCOUNT_ID = $accountID;
+
+        if (null == $appKey) $appKey = $this->APPLICATION_KEY;
+        else $this->APPLICATION_KEY = $appKey;
+
+        $auth = $this->api()->b2_authorize_account($accountID, $appKey);
+        $this->AUTH_TOKEN = $auth->authorizationToken;
+        $this->API_URL = $auth->apiUrl;
+        $this->DOWNLOAD_URL = $auth->downloadUrl;
+
+        return $auth;
     }
 
-    public function backup($aFile)
+    /**
+     * @param string $filePath
+     * @param null $bucketID
+     * @return mixed
+     */
+    public function backup($filePath, $bucketID = null)
     {
-       return $this->api()->b2_upload_file($this->uploadURL(), $this->AUTH_TOKEN, $aFile);
+        $this->initUploadURL($bucketID);
+        return $this->api()->b2_upload_file($this->UPLOAD_URL, $this->UPLOAD_AUTH_TOKEN, $filePath);
+    }
+
+    /**
+     * @param null $bucketID
+     */
+    public function initUploadURL($bucketID = null)
+    {
+        if (null != $this->UPLOAD_URL && null != $this->UPLOAD_AUTH_TOKEN) {
+            return;
+        }
+
+        // checks if already logged in B2 API
+        if (null == $this->AUTH_TOKEN || null == $this->API_URL) {
+            $this->authorize();
+        }
+
+        if (null != $bucketID) {
+            $this->BUCKET_ID = $bucketID;
+        }
+
+        $uploadURL = $this->api()->b2_get_upload_url($this->API_URL, $this->ACCOUNT_ID, $this->AUTH_TOKEN, $this->BUCKET_ID);
+        $this->UPLOAD_URL = $uploadURL->uploadUrl;
+        $this->UPLOAD_AUTH_TOKEN = $uploadURL->authorizationToken;
     }
 
 }
--- src/BackblazeServiceProvider.php
+++ src/BackblazeServiceProvider.php
@@ -1,11 +1,25 @@
 <?php
 
+/*
+ * This file is part of Laravel Junkies Backblaze.
+ *
+ * Copyright (c) 2016 Tobias Maxham <git2016@maxham.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ */
+
 namespace LaraJunkies\Backblaze;
 
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
 /**
+ * Class BackblazeServiceProvider
+ * @package LaraJunkies\Backblaze
+ *
  * @author Tobias Maxham <git2016@maxham.de>
  */
 class BackblazeServiceProvider extends ServiceProvider
@@ -24,7 +38,8 @@ class BackblazeServiceProvider extends ServiceProvider
     private function registerBackblaze($app)
     {
         $this->app->singleton('lj.backblaze', function (Container $app) {
-            return new BackblazeManager(new \b2_api());
+            $config = config('lj-backblaze');
+            return new BackblazeManager(new \b2_api(), $config);
         });
         $this->app->alias('lj.backblaze', BackblazeManager::class);
     }
