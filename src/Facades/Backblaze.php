79910bc669a98917409218fffe9b7bdad3910f9c
TobyMaxham <tobias.maxham@tricept.de>
2016-08-11 22:35:14 +0200

    Add Copyright

--- src/Facades/Backblaze.php
+++ src/Facades/Backblaze.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of Laravel Junkies Backblaze.
+ *
+ * Copyright (c) 2016 Tobias Maxham <git2016@maxham.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ */
+
 namespace LaraJunkies\Backblaze\Facades;
 
 use Illuminate\Support\Facades\Facade;
@@ -7,7 +18,8 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Backblaze
  * @package LaraJunkies\Backblaze\Facades
- * @author Tobias Maxham
+ *
+ * @author Tobias Maxham <git2016@maxham.de>
  */
 class Backblaze extends Facade
 {
